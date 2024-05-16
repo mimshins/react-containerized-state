cba2a031b3ec34174a046c92c35c7a406b1d9555
Mostafa Shamsitabar <50550858+mimshins@users.noreply.github.com>
2024-05-17 01:06:58 +0330

    [✨]: Add selectors api (#5)
    * Add external `use-sync-external-store` for backward compatibility issues
    
    * Opt-in new hook
    
    * Add selectors api
    
    * Update documentation

--- src/types.ts
+++ src/types.ts
@@ -7,4 +7,15 @@ export type CallableFunction<
 export type SubscribeCallback<T> = (value: T) => void;
 export type Unsubscribe = () => void;
 
+export type ValueSelector<T, P> = (value: T) => P;
+
+export type EqualityCheckFunction<P> = (a: P, b: P) => boolean;
+
+export type SelectedSubscribeEntry<T, P> = {
+  type: "SELECTED_SUBSCRIBE_ENTRY";
+  subscribeCallback: SubscribeCallback<P>;
+  selector: ValueSelector<T, P>;
+  isEqual?: EqualityCheckFunction<P>;
+};
+
 export type ContainerInitializer<T> = T | CallableFunction<[], T>;
