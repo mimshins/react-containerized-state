62f65ff047b809a1240e4a67c9ceab85ced0d4f6
mimshins <mostafa.sh.coderino@gmail.com>
2024-05-17 01:04:53 +0330

    Add selectors api

--- src/Container.ts
+++ src/Container.ts
@@ -3,15 +3,24 @@ import * as React from "react";
 import type {
   CallableFunction,
   ContainerInitializer,
+  EqualityCheckFunction,
+  SelectedSubscribeEntry,
   SubscribeCallback,
   Unsubscribe,
+  ValueSelector,
 } from "./types";
 import useLazyInitializedValue from "./use-lazy-initialized-value";
-import useSyncExternalStore from "./use-sync-external.store";
+import {
+  useSyncExternalStore,
+  useSyncExternalStoreWithSelector,
+} from "./use-sync-external-store";
 
 class Container<T> {
   private _value: T;
-  private _subscribers: Set<SubscribeCallback<T>>;
+  private _subscribers: Set<
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    SubscribeCallback<T> | SelectedSubscribeEntry<T, any>
+  >;
 
   constructor(initializer: ContainerInitializer<T>) {
     const initialValue =
@@ -24,13 +33,44 @@ class Container<T> {
   }
 
   /**
-   * Subscribes to the container changes and returns the unsubscribe function.
+   * Subscribes to the changes of the container's state value
+   * and returns the unsubscribe function.
    */
-  public subscribe(subscribeCallback: SubscribeCallback<T>) {
+  public subscribe(subscribeCallback: SubscribeCallback<T>): Unsubscribe {
     this._subscribers.add(subscribeCallback);
 
     const unsubscribe: Unsubscribe = () => {
       this._subscribers.delete(subscribeCallback);
+    };
+
+    return unsubscribe;
+  }
+
+  /**
+   * Subscribes to the changes of the container's selected state values
+   * and returns the unsubscribe function.
+   *
+   * For more control over emission changes, you may provide a custom equality function.
+   */
+  public selectedSubscribe<P>(
+    selector: ValueSelector<T, P>,
+    subscribeCallback: SubscribeCallback<P>,
+    /**
+     * A custom equality function to control emission changes.
+     */
+    isEqual?: EqualityCheckFunction<P>,
+  ): Unsubscribe {
+    const entry: SelectedSubscribeEntry<T, P> = {
+      type: "SELECTED_SUBSCRIBE_ENTRY",
+      selector,
+      subscribeCallback,
+      isEqual,
+    };
+
+    this._subscribers.add(entry);
+
+    const unsubscribe: Unsubscribe = () => {
+      this._subscribers.delete(entry);
     };
 
     return unsubscribe;
@@ -42,25 +82,49 @@ class Container<T> {
    * Please note that this function is not reactive!
    * Avoid using this in the React's rendering phase.
    */
-  public getValue() {
+  public getValue(): T {
     return this._value;
   }
 
   /**
    * Updates the value of the state and notifies the subscribers.
    */
-  public updateValue(newValue: T) {
+  public updateValue(newValue: T): void {
+    const prevValue = this._value;
+
     this._value = newValue;
 
-    this._subscribers.forEach(subscriber => subscriber(newValue));
+    this._subscribers.forEach(entry => {
+      if ("type" in entry) {
+        if (entry.type !== "SELECTED_SUBSCRIBE_ENTRY") return;
+
+        const { selector, subscribeCallback, isEqual } = entry;
+
+        const selectedValue = selector(newValue) as unknown;
+        const selectedPrevValue = selector(prevValue) as unknown;
+
+        const shouldEmit = !(
+          isEqual?.(selectedPrevValue, selectedValue) ??
+          Object.is(selectedValue, selectedPrevValue)
+        );
+
+        if (!shouldEmit) return;
+
+        subscribeCallback(selectedValue);
+      } else {
+        if (Object.is(prevValue, newValue)) return;
+
+        entry(newValue);
+      }
+    });
   }
 
   /**
    * A React hook to read the value of the state.
    *
-   * This is a reactive function and will be updated on state change.
+   * This is a reactive function and updates on state value change.
    */
-  public useValue() {
+  public useValue(): T {
     const storeSubscribe = useLazyInitializedValue(() =>
       this.subscribe.bind(this),
     );
@@ -75,6 +139,41 @@ class Container<T> {
       storeSubscribe,
       getClientSnapshot,
       getServerSnapshot,
+    );
+
+    return snapshot;
+  }
+
+  /**
+   * A React hook to read the values of the selected states.
+   *
+   * This is a reactive function and updates on selected state values change.
+   *
+   * For more control over re-rendering, you may provide a custom equality function.
+   */
+  public useValueSelector<P>(
+    selector: ValueSelector<T, P>,
+    /**
+     * A custom equality function to control re-rendering.
+     */
+    isEqual?: EqualityCheckFunction<P>,
+  ): P {
+    const storeSubscribe = useLazyInitializedValue(() =>
+      this.subscribe.bind(this),
+    );
+
+    const getClientSnapshot = useLazyInitializedValue(() =>
+      this.getValue.bind(this),
+    );
+
+    const getServerSnapshot = getClientSnapshot;
+
+    const snapshot = useSyncExternalStoreWithSelector(
+      storeSubscribe,
+      getClientSnapshot,
+      getServerSnapshot,
+      selector,
+      isEqual,
     );
 
     return snapshot;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 export { default as createStateContainer } from "./create-state-container";
+export type {
+  ContainerInitializer,
+  EqualityCheckFunction,
+  SubscribeCallback,
+  Unsubscribe,
+  ValueSelector,
+} from "./types";
