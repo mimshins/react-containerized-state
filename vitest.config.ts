f3decfe02be8be73a44498da5606858b2d97932d
mimshins <mostafa.sh.coderino@gmail.com>
2025-03-05 11:07:33 +0330

    update test coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 const config = defineConfig({
   plugins: [tsconfigPaths(), react()],
@@ -8,19 +8,15 @@ const config = defineConfig({
     environment: "jsdom",
     setupFiles: ["setupTests.ts"],
     include: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+    exclude: [...configDefaults.exclude, "**/__tests__/utils"],
     coverage: {
       provider: "v8",
-      reporter: ["text", "clover", "json-summary"],
+      reporter: ["text", "clover", "json-summary", "html"],
       include: ["src/**"],
-      exclude: [
-        "coverage/**",
-        "dist/**",
-        "**/*.d.ts",
-        "test?(s)/**",
-        "test?(-*).?(c|m)[jt]s?(x)",
-        "**/*{.,-}{test,spec}?(-d).?(c|m)[jt]s?(x)",
-        "**/__tests__/**",
-      ],
+      exclude: [...(configDefaults.coverage.exclude ?? []), "src/types.ts"],
+      thresholds: {
+        "100": true,
+      },
     },
   },
 });
